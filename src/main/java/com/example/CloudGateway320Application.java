6875bcaeef002afc5fa8a91b0a8811de21580636
Francis <franko200ng@gmail.com>
2023-12-25 15:37:32 +0100

    first commit

---
+++ src/main/java/com/example/CloudGateway320Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+public class CloudGateway320Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGateway320Application.class, args);
+	}
+
+}
